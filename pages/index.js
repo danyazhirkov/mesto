1eb53d5825a84d688f70f3db4ff0ffef1b21b1d7
Daniil Zhirkov <zhirkoffdaniil@gmail.com>
2023-01-19 11:27:42 +0400

    first commit

---
+++ pages/index.js
@@ -0,0 +1,30 @@
+let editButton = document.querySelector('.profile__edit-button');
+let popupContainer = document.querySelector('.popup');
+let closeButton = document.querySelector('.popup__close-button')
+let formElement = document.querySelector('.form__edit');
+let nameInput = document.querySelector('.form__item_input_name');
+let descriptionInput = document.querySelector('.form__item_input_description');
+let nameProfile = document.querySelector('.profile__name');
+let nameDescription = document.querySelector('.profile__description');
+editButton.addEventListener('click', getPopup); 
+closeButton.addEventListener('click', removePopup);
+
+
+function getPopup() {
+    popupContainer.classList.add('popup__opened')
+    nameInput.value = nameProfile.textContent;
+    descriptionInput.value = nameDescription.textContent; 
+}
+
+function removePopup() {
+    popupContainer.classList.remove('popup__opened')
+}
+
+function handleFormSubmit (evt) {
+    evt.preventDefault(); 
+    nameProfile.textContent = nameInput.value;
+    nameDescription.textContent = descriptionInput.value;
+    removePopup();
+}
+formElement.addEventListener('submit', handleFormSubmit); 
+
